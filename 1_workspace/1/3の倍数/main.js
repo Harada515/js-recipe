6342dfbdf4fa0fc3055d7529f0f2d7e81263e933
‘Harada515’ <naoki1561r@icloud>
2021-01-13 23:00:28 +0900

    1から指定した数までの数字を判定する関数を作る

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,9 +1,9 @@
-const number=24
-
+const genkiFunction =function(number){
 for(let n=1;n<=number;n++){
     if(n%3===0){
         console.log(n+"!!!!!!!")
     }else{
         console.log(n)
     }
+}
 }
