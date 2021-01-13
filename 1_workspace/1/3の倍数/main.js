c8c4c94cdd58950e6ed8db158ab9b2d17973d9d5
‘Harada515’ <naoki1561r@icloud>
2021-01-13 21:58:34 +0900

    1-24判定

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,7 +1,9 @@
 const number=24
 
-if (number%3===0){
-    console.log(number+"!!!!!!!")
-}else{
-    console.log(number)
+for(let n=1;n<=number;n++){
+    if(n%3===0){
+        console.log(n+"!!!!!!!")
+    }else{
+        console.log(n)
+    }
 }
