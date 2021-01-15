c252ce87a7e0f0029d3a65e292f1874d8f56c7c4
‘Harada515’ <naoki1561r@icloud>
2021-01-16 03:13:06 +0900

    いろんなイベント

--- 1_workspace/1/□と○/main.js
+++ 1_workspace/1/□と○/main.js
@@ -3,3 +3,10 @@ const figure = document.getElementById("figure")
 figure.onclick = function (){
     figure.classList.toggle('rounded') 
 }
+
+//いろんなイベント
+const figure2 = document.getElementById("figure2")
+
+figure2.onmouseover = function (){
+    figure2.classList.toggle('rounded')
+}
