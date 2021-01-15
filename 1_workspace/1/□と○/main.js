80b7892bbbcf971a6d9404b7c9f9906bf1fd916a
‘Harada515’ <naoki1561r@icloud>
2021-01-16 03:53:38 +0900

    △も！

--- 1_workspace/1/□と○/main.js
+++ 1_workspace/1/□と○/main.js
@@ -10,3 +10,19 @@ const figure2 = document.getElementById("figure2")
 figure2.onmouseover = function (){
     figure2.classList.toggle('rounded')
 }
+
+//△も！
+const figure3 = document.getElementById("figure3")
+let count=0
+figure3.onclick = function (){
+    count+=1
+    if(count%3===1){
+        figure3.classList.add('rounded')
+    }else if(count%3===2){
+        figure3.classList.remove('rounded')
+        figure3.classList.add('triangle')
+    }else{
+        figure3.classList.remove('triangle')
+    }
+    
+}
