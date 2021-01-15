cb4c0381359dba3e435875ac54d0fbc1a6711a51
‘Harada515’ <naoki1561r@icloud>
2021-01-16 04:14:22 +0900

    ストップウォッチ

---
+++ 1_workspace/1/ストップウォッチ/main.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display")
+const button = document.getElementById("button")
+
+let count=0
+
+const countUp=function(){
+    //countを更新
+    count+=1
+    console.log(count) // 追加
+    //count を秒単位にして表示
+    display.textContent=count/100
+}
+
+// 実行中のタイマーのid
+let id=null
+
+button.onclick = function(){
+    if (id === null) { // start
+        id = setInterval(countUp, 10)
+        button.textContent = "stop"
+    } else { // stop
+        clearInterval(id)
+        id = null
+        button.textContent = "start"
+    }
+}
