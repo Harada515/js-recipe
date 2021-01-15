86d8beb5ce195d16c1ec0f5ac901e74debf2d93d
‘Harada515’ <naoki1561r@icloud>
2021-01-16 02:39:39 +0900

    使いにくい電卓

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,7 +1,7 @@
-const display = document.getElementById("display")
-const plusButton = document.getElementById("plus-button")
-const minusButton = document.getElementById("minus-button")
-const twiceButton = document.getElementById("twice-button")
+const display = document.getElementById("display");
+const plusButton = document.getElementById("plus-button");
+const minusButton = document.getElementById("minus-button");
+const twiceButton = document.getElementById("twice-button");
 let count=0
 
 plusButton.onclick=function(){
@@ -15,4 +15,39 @@ minusButton.onclick=function(){
 twiceButton.onclick=function(){
     count=count*2
     display.textContent=count
+}
+//ここから電卓//
+const display2=document.getElementById("display2");
+const one=document.getElementById("one");
+const two=document.getElementById("two");
+const three=document.getElementById("three");
+const plus=document.getElementById("plus");
+const minus=document.getElementById("minus");
+const equal=document.getElementById("equal");
+let n=0
+let x=0
+
+one.onclick=function(){
+    x=1;
+    display2.textContent=1
+    }
+two.onclick=function(){
+    x=2;
+    display2.textContent=2
+    }
+three.onclick=function(){
+        x=3;
+        display2.textContent=3
+    }
+plus.onclick=function(){
+    n=n+x;
+    display2.textContent="+"
+    }
+minus.onclick=function(){
+    n=n-x;
+    display2.textContent="-"
+    }
+
+equal.onclick=function(){
+    display2.textContent=n
 }
