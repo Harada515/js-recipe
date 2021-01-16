cf2f6ce5f8497105678e94f28046bad4676f5713
‘Harada515’ <naoki1561r@icloud>
2021-01-16 18:19:03 +0900

    ３が大好き_完成

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,17 +1,25 @@
 const genkiFunction =function(number){
 for(let n=1;n<=number;n++){
-    if(n%3===0){
+       let num="00"+n
+       num=num.split("")
+    if(n%3===0||num.indexOf('3') != -1){
+        console.log(n + "!!!!!!!");
+    }else {
+        console.log(n);
+    }
+
+    /*else if(n%10===3){
+        console.log(n%10)
         console.log(n + "!!!!!!!")
-    }else if(n%10===3){
+    }else if((n/10)%10===3){
+        console.log((n/10)%10)
         console.log(n + "!!!!!!!")
-    }else if((n/10)%10==3){
+    }else if((n/100)%10===3){
         console.log(n + "!!!!!!!")
-    }else if((n/100)%10==3){
-        console.log(n + "!!!!!!!")
-    }else if((n/1000)%10==3){
+    }else if((n/1000)%10===3){
         console.log(n + "!!!!!!!")
     }else {
         console.log(n)
-    }
+    }*/
  }
 }
