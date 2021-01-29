422607fe4a97660e0be60f078f19b422afbeb295
‘Harada515’ <naoki1561r@icloud>
2021-01-30 03:02:22 +0900

    問７Week3小テスト

--- 1_workspace/1/小テストWeek3/main.js
+++ 1_workspace/1/小テストWeek3/main.js
@@ -1,3 +1,4 @@
+/*問６
 const toMorseCode = function(x) {
   let list = []
   let n = x.split("")
@@ -12,3 +13,17 @@ const toMorseCode = function(x) {
 }
 
 console.log(toMorseCode("000111000"))
+*/
+
+const addAllNumbers = function(x) {
+  let n = x.split("-") //["12","34","5"]
+  let num = n.map(Number)
+  let add = 0
+  for (let i = 0; i < n.length; i++)
+    if (i < n.length) {
+      add = add + num[i]
+    }
+  return add
+}
+
+console.log(addAllNumbers("12-34-5"))
