546e2270317d15f5b04dfce1ee19be4f9c9c30af
‘Harada515’ <naoki1561r@icloud>
2021-01-30 12:53:34 +0900

    元気な秒針

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,3 +1,32 @@
+const button = document.getElementById("button")
+
+let count = 0
+
+const countUp = function() {
+  count += 1
+  let num = "00" + count
+  num = num.split("")
+  if (count % 3 === 0 || num.indexOf("3") != -1) {
+    console.log(count + "!!!!!!!")
+  } else {
+    console.log(count)
+  }
+}
+
+let id = null
+
+button.onclick = function() {
+  if (id === null) {
+    id = setInterval(countUp, 1000)
+    button.textContent = "stop"
+  } else {
+    clearInterval(id)
+    id = null
+    button.textContent = "start"
+  }
+}
+
+/*３が大好き
 const genkiFunction =function(number){
 for(let n=1;n<=number;n++){
        let num="00"+n
@@ -6,9 +35,10 @@ for(let n=1;n<=number;n++){
         console.log(n + "!!!!!!!");
     }else {
         console.log(n);
-    }
+    }*/
 
-    /*else if(n%10===3){
+/*３が大好き
+    else if(n%10===3){
         console.log(n%10)
         console.log(n + "!!!!!!!")
     }else if((n/10)%10===3){
@@ -21,5 +51,3 @@ for(let n=1;n<=number;n++){
     }else {
         console.log(n)
     }*/
- }
-}
